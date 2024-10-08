096d78749456f1aa35ead3125a992d5f996ba43a
Denis Zagvozdin <gaz91.91@mail.ru>
2024-10-08 17:47:46 +0500

    fix: change http header

--- api/json.go
+++ api/json.go
@@ -14,7 +14,7 @@ func initValidator() {
 }
 
 func writeJSON(w http.ResponseWriter, status int, data any) error {
-	w.Header().Set("Content-Type", "Application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(data)
 }
